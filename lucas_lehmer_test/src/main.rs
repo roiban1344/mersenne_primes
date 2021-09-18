62f4345e1a62376373b086eaf75e1aaf25d4f3f1
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-09-19 02:51:56 +0900

    Improve the modulo operation

--- lucas_lehmer_test/src/main.rs
+++ lucas_lehmer_test/src/main.rs
@@ -1,7 +1,7 @@
 use num::bigint::BigInt;
-use num::Signed;
 use num::bigint::Sign;
 use num::integer;
+use num::Signed;
 
 fn is_prime(n: u32) -> bool {
     if n == 0 {
@@ -17,7 +17,7 @@ fn is_prime(n: u32) -> bool {
     true
 }
 
-fn mersenne_number(n: u32) -> BigInt{
+fn mersenne_number(n: u32) -> BigInt {
     (num::one::<BigInt>() << n) - num::one::<BigInt>()
 }
 
@@ -25,16 +25,26 @@ fn lucas_lehmer_test(p: u32) -> bool {
     //p must be an odd prime
     let m = mersenne_number(p);
     let mut s = BigInt::new(Sign::Plus, vec![4]);
-    for _ in 1..=p-2{
+    for _ in 1..=p - 2 {
         s = &s * &s - BigInt::new(Sign::Plus, vec![2]);
-        s %= &m;
+        if s.is_negative() {
+            s += &m;
+        }
+        while s >= m {
+            let a = (&s >> p) << p;
+            s = (&a >> p) + (a ^ s);
+            if s == m {
+                s = num::zero();
+                break;
+            }
+        }
     }
     s == num::zero()
-}   
+}
 
 fn main() {
     for i in 3..1000 {
-        if !is_prime(i){
+        if !is_prime(i) {
             continue;
         }
         if lucas_lehmer_test(i) {
