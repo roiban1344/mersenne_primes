b89a4dc115d7cef5fe3b434aae9086749ce3ce36
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-09-19 02:41:54 +0900

    Implement Lucas Lehmer test

---
+++ lucas_lehmer_test/src/main.rs
@@ -0,0 +1,44 @@
+use num::bigint::BigInt;
+use num::Signed;
+use num::bigint::Sign;
+use num::integer;
+
+fn is_prime(n: u32) -> bool {
+    if n == 0 {
+        panic!();
+    } else if n == 1 {
+        return false;
+    }
+    for d in 2..=integer::sqrt(n) {
+        if n % d == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn mersenne_number(n: u32) -> BigInt{
+    (num::one::<BigInt>() << n) - num::one::<BigInt>()
+}
+
+fn lucas_lehmer_test(p: u32) -> bool {
+    //p must be an odd prime
+    let m = mersenne_number(p);
+    let mut s = BigInt::new(Sign::Plus, vec![4]);
+    for _ in 1..=p-2{
+        s = &s * &s - BigInt::new(Sign::Plus, vec![2]);
+        s %= &m;
+    }
+    s == num::zero()
+}   
+
+fn main() {
+    for i in 3..1000 {
+        if !is_prime(i){
+            continue;
+        }
+        if lucas_lehmer_test(i) {
+            println!("{}", i)
+        }
+    }
+}
