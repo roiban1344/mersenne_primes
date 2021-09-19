2ae5a9cebd5ea33d0a0de671215167f17abc484d
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-09-19 12:28:40 +0900

    Replace is_prime with sieve to generate primes

--- lucas_lehmer_test/src/main.rs
+++ lucas_lehmer_test/src/main.rs
@@ -1,20 +1,23 @@
 use num::bigint::BigInt;
 use num::bigint::Sign;
-use num::integer;
-use num::Signed;
+use num::{Signed, Zero};
 
-fn is_prime(n: u32) -> bool {
-    if n == 0 {
-        panic!();
-    } else if n == 1 {
-        return false;
-    }
-    for d in 2..=integer::sqrt(n) {
-        if n % d == 0 {
-            return false;
+fn sieve(n:u32, only_odd:bool)->Vec<u32>{
+    let mut is_prime = vec![true;(n+1) as usize];
+    let mut p = Vec::<u32>::new();
+    for d in 2..=n{
+        if is_prime[d as usize] {
+            if d >=3 || !only_odd {
+                p.push(d);
+            }
+            let mut k = 2 * d;
+            while k <= n {
+                is_prime[k as usize] = false;
+                k += d;
+            }
         }
     }
-    true
+    p
 }
 
 fn mersenne_number(n: u32) -> BigInt {
@@ -24,29 +27,30 @@ fn mersenne_number(n: u32) -> BigInt {
 fn lucas_lehmer_test(p: u32) -> bool {
     //p must be an odd prime
     let m = mersenne_number(p);
+    let two = BigInt::new(Sign::Plus, vec![2]);
     let mut s = BigInt::new(Sign::Plus, vec![4]);
     for _ in 1..=p - 2 {
-        s = &s * &s - BigInt::new(Sign::Plus, vec![2]);
+        s = &s * &s - &two;
         if s.is_negative() {
             s += &m;
         }
         while s >= m {
-            let a = (&s >> p) << p;
-            s = (&a >> p) + (a ^ s);
+            s = (&s >> p) + (s & &m);
             if s == m {
                 s = num::zero();
                 break;
             }
         }
+        // if i < p-2 && s.is_zero() {
+        //     println!("{} {}", p, i);
+        // }
     }
-    s == num::zero()
+    s.is_zero()
 }
 
 fn main() {
-    for i in 3..1000 {
-        if !is_prime(i) {
-            continue;
-        }
+    let p = sieve(30000, true);
+    for i in p {
         if lucas_lehmer_test(i) {
             println!("{}", i)
         }
