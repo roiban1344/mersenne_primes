40a82269f9612c5becc1bb56c80cf2f5e33c7ebf
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-09-20 23:39:03 +0900

    Add a very simple code to show max values of the signed integer types

---
+++ min_max/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("{} {}", i8::BITS, i8::MAX);
+    println!("{} {}", i16::BITS, i16::MAX);
+    println!("{} {}", i32::BITS, i32::MAX);
+    println!("{} {}", i64::BITS, i64::MAX);
+    println!("{} {}", i128::BITS, i128::MAX);
+}
