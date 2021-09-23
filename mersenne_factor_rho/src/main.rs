71ab086324db8d4fd6352b2a166d214eed3e6f99
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2021-09-23 19:02:11 +0900

    Improve rho method

--- mersenne_factor_rho/src/main.rs
+++ mersenne_factor_rho/src/main.rs
@@ -1,44 +1,73 @@
 use rug::ops::Pow;
 use rug::{integer::IsPrime, Integer};
+use std::collections::VecDeque;
 
-// fn divmod_by_mersenne(&n: &Integer, k: u32) -> Integer {
-//     let mut n = n;
-//     let m = (Integer::from(2) << k) - 1;
-//     while n >= m {
-//         n = Integer::from(&n >> k) + Integer::from(n & &m);
-//         if n == m {
-//             n = Integer::from(0);
-//             break;
-//         }
-//     }
-//     n
-// }
+const MILLER_RABIN_REPS: u32 = 64;
 
-fn gen_pseudo_rand(x: Integer, modulo: &Integer) -> Integer {
-    x % modulo
+fn odd_primes(n: u32) -> Vec<u32> {
+    let size = ((n - 1) >> 1) as usize;
+    let mut is_prime = vec![true; size];
+    let mut primes = vec![];
+    for i in 0..size {
+        if is_prime[i] {
+            let p = 2 * i + 3;
+            primes.push(p as u32);
+            let mut j = 3 * i + 3;
+            while j < size {
+                is_prime[j] = false;
+                j += p;
+            }
+        }
+    }
+    primes
 }
 
 fn main() {
-    let k = 101;
-    let mut m: Integer = (Integer::from(1) << k) - 1;
-    println!("M_{} {}", k, m);
-    while m.is_probably_prime(64) == IsPrime::No {
-        let mut x = Integer::from(2);
-        let mut y = Integer::from(2);
-        let mut d = Integer::from(1);
-        while d == 1 {
-            x = gen_pseudo_rand(x.pow(2) + 1, &m);
-            y = gen_pseudo_rand(y.pow(2) + 1, &m);
-            y = gen_pseudo_rand(y.pow(2) + 1, &m);
-            let diff = Integer::from(&x - &y).abs();
-            d = diff.gcd(&m);
+    let primes = odd_primes(128);
+    for p in primes {
+        let mut stack = VecDeque::<Integer>::new();
+        let mut factors = Vec::<Integer>::new();
+        let m: Integer = (Integer::from(1) << p) - 1;
+        stack.push_back(m);
+        while stack.len() > 0 {
+            let mut n = stack.pop_front().unwrap();
+            let mut c = 1;
+            while n.is_probably_prime(MILLER_RABIN_REPS) == IsPrime::No {
+                let mut x = Integer::from(2);
+                let mut y = Integer::from(2);
+                let mut d = Integer::from(1);
+                while d == 1 {
+                    macro_rules! rand_next {
+                        ($x:expr) => {
+                            ($x.pow(2) + c) % &n
+                        };
+                    }
+                    x = rand_next!(x);
+                    y = rand_next!(y);
+                    y = rand_next!(y);
+                    let diff = Integer::from(&x - &y).abs();
+                    d = diff.gcd(&n);
+                }
+                if d == n {
+                    c += 1;
+                    continue;
+                } else {
+                    n /= &d;
+                    if d.is_probably_prime(MILLER_RABIN_REPS) == IsPrime::No {
+                        stack.push_back(d);
+                    } else {
+                        factors.push(d);
+                    }
+                }
+            }
+            factors.push(n);
         }
-        if d == m {
-            continue;
-        } else {
-            println!("{}", d);
-            m /= d;
-        }
+        factors.sort();
+        let factorization = factors
+            .into_iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<_>>()
+            .join(" * ");
+        println!("M_{} = {}", p, factorization);
     }
-    println!("{}", m);
 }
